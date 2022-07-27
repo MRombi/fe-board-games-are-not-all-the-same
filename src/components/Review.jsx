62586c7ed61d3258874a022c861f38dd46eb9638
MRombi <michael.rombi@hotmail.com>
2022-07-27 15:09:08 +0100

    add two buttons to vote on the review

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -1,25 +1,58 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { Link, useParams, useLocation } from "react-router-dom";
+import { useParams } from "react-router-dom";
 
 const Review = () => {
-  // const stateParam = useLocation()
-  // const stateParamVal = useLocation.state
-  console.log(useLocation());
-  // let location = useLocation()
-
-  // console.log(location);
   const [review, setReview] = useState([]);
- 
+  const [votes, setVotes] = useState();
+  const [hasUpvoted, setHasUpvoted] = useState(false);
+  const [hasDownvoted, setHasDownvoted] = useState(false);
+
   let id = useParams().review_id;
   useEffect(() => {
     fetchReview();
   }, []);
+
   const fetchReview = async () => {
     const data = await axios.get(
       `https://board-games-are-not-the-sames.herokuapp.com/api/reviews/${id}`
     );
     setReview(data.data.review);
+    setVotes(data.data.review.votes);
+  };
+
+  const handleVoteIncrease = () => {
+    setVotes((currVotes) => {
+      if (!hasUpvoted) {
+        setHasUpvoted(true);
+        setHasDownvoted(false);
+        const data = axios.patch(
+          `https://board-games-are-not-the-sames.herokuapp.com/api/reviews/${id}`,
+          { inc_votes: 1 }
+        );
+        return currVotes + 1;
+      } else {
+        alert("You have already Upvoted");
+        return currVotes;
+      }
+    });
+  };
+
+  const handleVoteDecrease = () => {
+    setVotes((currVotes) => {
+      if (!hasDownvoted) {
+        setHasDownvoted(true);
+        setHasUpvoted(false);
+        const data = axios.patch(
+          `https://board-games-are-not-the-sames.herokuapp.com/api/reviews/${id}`,
+          { inc_votes: -1 }
+        );
+        return currVotes - 1;
+      } else {
+        alert("You have already Downvoted");
+        return currVotes;
+      }
+    });
   };
 
   return (
@@ -30,18 +63,22 @@ const Review = () => {
           <img className="images" src={review.review_img_url} />
         </div>
         <div className="review-data">
-        <time dateTime={review.created_at}>
-        Created the:{" "}
-                {review.created_at}
-            
+          <time dateTime={review.created_at}>
+            Created the: {review.created_at}
           </time>
-          <ul>
-            <li>Comment Count: {review.comment_count}</li>
-            <li>Votes: {review.votes}</li>
-          </ul>
-          <p className="review-body">
-          {review.review_body}
-          </p>
+          <div className="review-comment-count-container">
+            Comment Count: {review.comment_count}
+          </div>
+          <div className="review-votes-container">
+            Votes: {votes}{" "}
+            <button className="button-increase" onClick={handleVoteIncrease}>
+              Upvote
+            </button>{" "}
+            <button className="button-decrease" onClick={handleVoteDecrease}>
+              Downvote
+            </button>
+          </div>
+          <p className="review-body">{review.review_body}</p>
         </div>
       </article>
     </div>
