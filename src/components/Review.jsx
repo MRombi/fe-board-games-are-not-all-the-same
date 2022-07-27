7c4c1f50c4c9e756a623dcebf3136f25caf73360
MRombi <michael.rombi@hotmail.com>
2022-07-27 10:49:20 +0100

    add a single review page with relevant information

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -1,11 +1,51 @@
 import React, { useState, useEffect } from "react";
-
-import {Link, useParams, useLocation} from "react-router-dom";
+import axios from "axios";
+import { Link, useParams, useLocation } from "react-router-dom";
 
 const Review = () => {
-  return (
-    <div>{useParams().review_id}</div>
-  )
-}
+  // const stateParam = useLocation()
+  // const stateParamVal = useLocation.state
+  console.log(useLocation());
+  // let location = useLocation()
 
-export default Review
+  // console.log(location);
+  const [review, setReview] = useState([]);
+ 
+  let id = useParams().review_id;
+  useEffect(() => {
+    fetchReview();
+  }, []);
+  const fetchReview = async () => {
+    const data = await axios.get(
+      `https://board-games-are-not-the-sames.herokuapp.com/api/reviews/${id}`
+    );
+    setReview(data.data.review);
+  };
+
+  return (
+    <div>
+      <article className="review-card">
+        <h4>{review.title}</h4>
+        <div className="review-card-media">
+          <img className="images" src={review.review_img_url} />
+        </div>
+        <div className="review-data">
+        <time dateTime={review.created_at}>
+        Created the:{" "}
+                {review.created_at}
+            
+          </time>
+          <ul>
+            <li>Comment Count: {review.comment_count}</li>
+            <li>Votes: {review.votes}</li>
+          </ul>
+          <p className="review-body">
+          {review.review_body}
+          </p>
+        </div>
+      </article>
+    </div>
+  );
+};
+
+export default Review;
