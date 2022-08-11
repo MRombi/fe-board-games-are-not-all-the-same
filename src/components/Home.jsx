1b48f73aaa4a6dba0b2d712fa50fc7bfb23f45b5
MRombi <michael.rombi@hotmail.com>
2022-08-11 18:22:53 +0100

    add an avatar selection in home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from "react";
 import { useContext } from "react";
 import { UserContext } from "./context/User";
+import { Link } from "react-router-dom";
 import axios from "axios";
 
 const Home = () => {
   const { username, setUsername } = useContext(UserContext);
-  const [user, setUser] = useState([])
+  const [isLoading, setIsLoading] = useState(true);
+  const [users, setUsers] = useState([]);
   useEffect(() => {
     fetchUsers();
   }, []);
@@ -13,17 +15,35 @@ const Home = () => {
     const data = await axios.get(
       `https://board-games-are-not-the-sames.herokuapp.com/api/users`
     );
-    setUser(data.data.users[0])
-  }
-    
+    setUsers(data.data.users);
+    setIsLoading(false);
+  };
+
   return (
-    <div className="home-container">
-      <h2>Welcome back {user.name}</h2>
-      <div className="avatar-container">
-      <img className="avatar" src={user.avatar_url} alt="Avatar with wobbly arms and a blue hat" />
-      <h4>{username} 's avatar</h4>
+    <div className="home">
+      {isLoading && <h3 className="loading">Loading...</h3>}
+      <h2>Choose your Avatar!</h2>
+      <div className="home-container">
+        {users.map((user) => {
+          return (
+            <div className="profile-container" key={user.username}>
+              <Link
+                to={{
+                  pathname: `/reviews`,
+                }}
+                onClick={setUsername(user.username)}
+              >
+                <h3>{user.username}</h3>
+              </Link>
+              <img
+                className="avatar"
+                src={user.avatar_url}
+                alt={user.username}
+              />
+            </div>
+          );
+        })}
       </div>
-      <h3 className="prolific">Our Most Prolific Review Writer!</h3>
     </div>
   );
 };
